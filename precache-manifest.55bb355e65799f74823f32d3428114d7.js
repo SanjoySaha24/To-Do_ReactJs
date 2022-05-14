2a3bd33ae80575bdaf6065c65b41d7a9bf823e26
Sanjoy Saha <76898740+SanjoySaha24@users.noreply.github.com>
2022-05-14 21:04:34 +0530

    Updates

---
+++ precache-manifest.55bb355e65799f74823f32d3428114d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c636ca254da724dcd18c964297b21f68",
+    "url": "/To-Do_ReactJs/index.html"
+  },
+  {
+    "revision": "273344925306a0f78023",
+    "url": "/To-Do_ReactJs/static/css/main.1e0d6d27.chunk.css"
+  },
+  {
+    "revision": "fe0148b8a82147ece620",
+    "url": "/To-Do_ReactJs/static/js/2.4d4bc6f1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/To-Do_ReactJs/static/js/2.4d4bc6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273344925306a0f78023",
+    "url": "/To-Do_ReactJs/static/js/main.e97e2f11.chunk.js"
+  },
+  {
+    "revision": "dba88d980fc26e8d18a4",
+    "url": "/To-Do_ReactJs/static/js/runtime-main.e1926db3.js"
+  }
+]);
